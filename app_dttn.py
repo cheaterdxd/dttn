8c33bf526c8439116b8d22656612efcea7726325
cheaterdxd <thanhtuan9906@gmail.com>
2021-11-21 22:57:21 -0800

    update some other func

--- app_dttn.py
+++ app_dttn.py
@@ -1,8 +1,9 @@
 from menu_list import cve_menu, main_menu
-import signal,sys
 from findInfoSystem import get_system_info
 from util import clean_terminal
 import importlib
+from importFile import sys, signal
+from formatOutput import prettyAnnounce
 
 def signal_handler(signal, frame):
     print('\nBạn đã thoát chương trình bằng Ctrl+C. Hẹn gặp lại. ')
@@ -21,8 +22,25 @@ def do_cve(cve_name):
     cl = (run_entry_for_cve(f'linux.{cve_name}.{cve_name}'))
     cl() # call cve entry
 
+def check_before_run():
+    '''
+    Co dung phien ban python3.6 
+    Co cai pwntools chua? 
+    '''
+    ver = sys.version.split(" ")[0].split('.')
+    print(ver)
+    num1_ver = ver[0]
+    num2_ver = ver[1]
+    num3_ver = ver[2]
+    if(num1_ver!='3'):
+        prettyAnnounce.log.warning("vui lòng nâng cấp lên python 3, tốt nhất là từ 3.6.9 trở lên")
+    elif(num2_ver<'6'):
+        prettyAnnounce.log.warning("chương trình có thể chạy không chính xác ở phiên bẳn python hiện tại, tốt nhất là từ 3.6.9 trở lên")
+    elif(num3_ver<'9'):
+        prettyAnnounce.log.warning("chương trình có thể chạy không chính xác ở phiên bẳn python hiện tại, tốt nhất là từ 3.6.9 trở lên")
 def main():
     signal.signal(signal.SIGINT, signal_handler)
+    check_before_run()
     while(1):
         clean_terminal()
         choice = main_menu()
--- importFile.py
+++ importFile.py
@@ -5,6 +5,7 @@ import logging
 import sys, os
 import subprocess 
 import shlex
+import signal
 from pwn import *
 
 global root_directory, os_type
